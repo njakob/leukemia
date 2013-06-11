ec37da9cc9d0c3e55899a739ca70fb2664859be0
landart <correo@jorgealbaladejo.com>
2013-06-11 11:15:23 +0200

    GA is binary

--- main.R
+++ main.R
@@ -1,31 +1,43 @@
 ### dependencies
-install.packages("GA_1.1", type="source");
+#install.packages("GA_1.1", type="source");
 library("GA");
 
 ### params
 datasetSize <- 1000;
+maxGeneSelectionSize <- 50;
 
 ### load data
-input <- data.frame(
+data <- data.frame(
   read.csv(
-    paste("/media/KINGSTON/MLBD/allDataNoNAClass",datasetSize,".txt",sep=""),
+    paste("data/allDataNoNAClass",datasetSize,".txt",sep=""),
     sep=""
   )
 );
+input <- data[,1:1000]
+classes <- data[,1001]
 
 ### genetic wrapper
 
 ## evaluation function
-evaluate <- function(gene) {
-  print(gene)
-  1
+evaluate <- function(x) {
+  # x <- c(1,0,1,....) # size limit set before
+  if (sum(x) > 50)
+    return (0) # TODO: set a regressive formula
+  else
+    return(1);
+  
+  # genes <- dataset[,x=1]
+  
+  # current_solution_survivalpoints <- x %*% input[1,]
+  #survivalpoints current_solution_weight <- x %*% dataset$weight  if (current_solution_weight > weightlimit)  return(0) else return(-current_solution_survivalpoints)
 }
 
 ## Launch genetic algorithm
 GA <- ga(
-  "permutation",
-  min = 0,
-  max = 1000,
+  "binary",
+  nBits = ncol(input),
+  #min = 0,
+  #max = 1000,
   fitness = evaluate,
   maxiter = 1000,
   run = 200,
